9eb8618601acc72bc61ee142b56af700fbc403f4
Alejandro Cabeza <alejandrocabeza400@gmail.com>
2026-06-20 09:58:05 -0400

    remove old config of hyprland

--- hypr/lua/env.lua
+++
@@ -1,4 +0,0 @@
--- Variables de entorno
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
-hl.env("GDK_SCALE", "1")
--- hypr/lua/programs.lua
+++
@@ -1,6 +0,0 @@
--- Programas que usa el sistema
-return {
-    terminal    = "alacritty",
-    fileManager = "dolphin",
-    menu        = "walker",
-}
